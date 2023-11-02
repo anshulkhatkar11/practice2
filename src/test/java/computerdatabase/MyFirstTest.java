759cca9f23be199a52422f720d9e8d917133b6e6
anshulkhatkar111 <vloggers2024@gmail.com>
2023-11-02 12:02:47 +0000

    First commit

---
+++ src/test/java/computerdatabase/MyFirstTest.java
@@ -0,0 +1,34 @@
+package computerdatabase;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class MyFirstTest extends Simulation {
+
+    // 1 Http Configuration
+    private HttpProtocolBuilder httpProtocol = http
+            .baseUrl("http://localhost:9091")
+            .acceptHeader("application/json");
+
+    // 2 Scenario Definition
+    private ScenarioBuilder scn = scenario("My First Test")
+            .exec(http("Get all games")
+                    .get("/hello")
+                    .check(
+                            status().is(200), // Check if the response status code is 200 (OK)
+                            regex("helloji").find(0).exists() // Check if the response body contains "Expected Text"
+                    )
+            );
+
+    // 3 Load Simulation
+    {
+        setUp(
+                scn.injectOpen(atOnceUsers(1))
+        ).protocols(httpProtocol);
+    }
+}
