c40bcd19cad439b03a800cc42cb239074d65bbb4
anshulkhatkar111 <vloggers2024@gmail.com>
2023-11-06 11:18:59 +0000

    Initial commit

--- src/test/java/computerdatabase/MyFirstTest.java
+++ src/test/java/computerdatabase/MyFirstTest.java
@@ -12,17 +12,14 @@ public class MyFirstTest extends Simulation {
 
     // 1 Http Configuration
     private HttpProtocolBuilder httpProtocol = http
-            .baseUrl("http://localhost:9092")
+            .baseUrl("http://computer-database.gatling.io")
             .acceptHeader("application/json");
 
     // 2 Scenario Definition
     private ScenarioBuilder scn = scenario("My First Test")
             .exec(http("Get all games")
-                    .get("/hello")
-                    .check(
-                            status().is(200), // Check if the response status code is 200 (OK)
-                            regex("helloji").find(0).exists() // Check if the response body contains "Expected Text"
-                    )
+                    .get("/")
+
             );
 
     // 3 Load Simulation
